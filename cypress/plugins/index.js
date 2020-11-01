5693a2efbc0831fa8f284c5dd4e8274f4d8c5dfc
Sin <48003401+Sinnv2710@users.noreply.github.com>
2020-11-01 18:37:24 +0700

    update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,7 +11,16 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const pgp = require('pg-promise')();
-const postgressConfig = require(require('path').resolve('cypress.json'));
+if ( file === "dev") {
+  const postgressConfig = require(require('path').resolve('cypress/plugins/db/dev.json'));
+} else if (file === "stage")
+{
+  const postgressConfig = require(require('path').resolve('cypress/plugins/db/stage.json'));
+}
+else if (file === "production") 
+{
+  const postgressConfig = require(require('path').resolve('cypress/plugins/db/production.json'));
+}
 
 module.exports =  function(query,userDefineConnection)  {
   let connection = postgressConfig.db
