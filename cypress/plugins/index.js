be33b6d9282207203908ab1d31fd2b84aebbfc44
Sin <48003401+Sinnv2710@users.noreply.github.com>
2020-11-01 19:03:55 +0700

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,25 +11,38 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const pgp = require('pg-promise')();
-let environment;
-if ( environment === "dev") {
-  const postgressConfig = require(require('path').resolve('cypress/plugins/db/dev.json'));
-} else if (environment === "stage")
-{
-  const postgressConfig = require(require('path').resolve('cypress/plugins/db/stage.json'));
-}
-else if (environment === "production") 
-{
-  const postgressConfig = require(require('path').resolve('cypress/plugins/db/production.json'));
-}
+let dev_path = "cypress/plugins/db/dev.json";
+let stage_path = "cypress/plugins/db/stage.json";
+let production_path = "cypress/plugins/db/production.json"
 
 module.exports =  function(query,userDefineConnection,environment)  {
-  let connection = postgressConfig.db
-  if (userDefineConnection!=undefined){
-    connection=userDefineConnection
+  if (environment === 'dev') {
+    const postgressConfig = require(require('path').resolve(dev_path));
+    let connection = postgressConfig.db
+    if (userDefineConnection != undefined) {
+      connection = userDefineConnection
+    }
+    const db = pgp(connection);
+    return db.any(query)
+  } else if (environment === 'stage') {
+    const postgressConfig = require(require('path').resolve(stage_path));
+
+    let connection = postgressConfig.db
+    if (userDefineConnection != undefined) {
+      connection = userDefineConnection
+    }
+    const db = pgp(connection);
+    return db.any(query)
+  } else if (environment === 'production') {
+    const postgressConfig = require(require('path').resolve(production_path));
+
+    let connection = postgressConfig.db
+    if (userDefineConnection != undefined) {
+      connection = userDefineConnection
+    }
+    const db = pgp(connection);
+    return db.any(query)
   }
-  const db = pgp(connection);
-  return db.any(query)
 }
 
 
